5c5d029099a1908fcc35046a91737a8b618d4bc4
maurobuitrago <maob14@gmail.com>
2022-11-24 11:21:13 -0500

    creacion escenario resta

--- src/test/java/com/co/bootcamp/stepsdefinitions/CalculatorStepDefinition.java
+++ src/test/java/com/co/bootcamp/stepsdefinitions/CalculatorStepDefinition.java
@@ -1,14 +1,18 @@
 package com.co.bootcamp.stepsdefinitions;
 
+import com.co.bootcamp.questions.CaptureResult;
+import com.co.bootcamp.tasks.Calculator;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.thucydides.core.annotations.Managed;
+import org.hamcrest.Matchers;
 import org.openqa.selenium.WebDriver;
 
 public class CalculatorStepDefinition {
@@ -30,10 +34,14 @@ public class CalculatorStepDefinition {
 
     @When("^he user made a Minimun$")
     public void heUserMadeAMinimun() {
-    }
+        OnStage.theActorInTheSpotlight().attemptsTo(Calculator.calculate());
+
+            }
 
     @Then("^he user could see the result$")
     public void heUserCouldSeeTheResult() {
+    OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(CaptureResult.validate()
+    , Matchers.equalTo(4)));
     }
 
 }
